6172f6c35572add47de743710504704bf6d43ac8
vechkitov <vechkitov@bk.ru>
2022-05-22 12:18:29 +0200

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private static final String SKIPPED_PATTERN = "^(\\s*#.*|\\s*$)";
+    private static final String VALID_PATTERN = "\\S+=.*";
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.matches(SKIPPED_PATTERN)) {
+                    continue;
+                }
+                if (!line.matches(VALID_PATTERN)) {
+                    throw new IllegalArgumentException("Не соответствие формату \"ключ=значение\": " + line);
+                }
+                String[] pair = line.split("=", 2);
+                values.put(pair[0], pair[1].isBlank() ? null : pair[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        values.forEach((k, v) -> out.add(k + "=" + v));
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("app.properties");
+        config.load();
+        System.out.println(config);
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        Config config = new Config("./data/pair_without_comment.properties");
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(nullValue()));
+    }
+
+    @Test
+    public void whenPairWithCommentAndEmptyLines() {
+        Config config = new Config("./data/pair_with_comment_and_empty_lines.properties");
+        config.load();
+        assertThat(config.toString().lines().count(), is(2L));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairWithoutKey() {
+        new Config("./data/pair_without_key.properties").load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairDoesNotHaveEqualSing() {
+        new Config("./data/pair_without_equal_sing.properties").load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairHasOnlyEqualSing() {
+        new Config("./data/pair_with_only_equal_sing.properties").load();
+    }
+}
