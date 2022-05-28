fc9bdcc48af28b3f5f6fcc00588b8a4caeefa6b3
vechkitov <vechkitov@bk.ru>
2022-05-28 12:53:17 +0200

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,8 +11,9 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("d:\\job4j\\projects\\job4j_design\\");
-        search(start, p -> p.toFile().getName().endsWith(".java"))
+        validateParams(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1]))
                 .forEach(System.out::println);
     }
 
@@ -19,5 +21,31 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validateParams(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Программа должна принимать 2 параметра: начальный"
+                    + " каталог и расширение файла. Например: java -jar search.jar c:\\temp .java");
+        }
+        File rootFolder = new File(args[0]);
+        if (!rootFolder.exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Неверно указан 1-й параметр: каталога '%s' не существует.",
+                            rootFolder.getAbsoluteFile())
+            );
+        }
+        if (!rootFolder.isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Неверно указан 1-й параметр: '%s' не является каталогом.",
+                            rootFolder.getAbsoluteFile())
+            );
+        }
+        if (!args[1].matches("\\..+")) {
+            throw new IllegalArgumentException(
+                    String.format("Неверно указан 2-й параметр: расширение файла должно иметь вид '.*'. "
+                            + "Например: .java", rootFolder.getAbsoluteFile())
+            );
+        }
     }
 }
