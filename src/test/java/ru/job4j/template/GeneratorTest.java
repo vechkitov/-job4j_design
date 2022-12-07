32e5824f378e7bbc7d9101f3317938a1472aa075
vechkitov <vechkitov@bk.ru>
2022-12-07 09:00:56 +0200

    3. Шаблонизатор. [#855 #368450]: для тестов добавлена аннотация @Disabled

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.template;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Disabled
 class GeneratorTest {
 
     @Test
