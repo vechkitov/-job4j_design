2590148c7f9919686a347e912e9ce22b168af4fb
vechkitov <vechkitov@bk.ru>
2023-03-22 22:12:33 +0200

    1. Хранилище продуктов [#852 #403382]

--- src/test/java/ru/job4j/ood/lsp/ControlQualityTest.java
+++ src/test/java/ru/job4j/ood/lsp/ControlQualityTest.java
@@ -53,33 +53,6 @@ class ControlQualityTest {
         assertThat(trash.getFoods().size()).isEqualTo(0);
     }
 
-//    @Test
-//    void whenPercentOfUsedShelfLifeIsEqualsTo75() {
-//        var createDate = LocalDateTime.of(2000, 1, 1, 12, 0);
-//        var expiryDate = LocalDateTime.of(2000, 1, 21, 12, 0);
-//        var date = LocalDateTime.of(2000, 1, 16, 12, 0);
-//        var food = new Food("foo", createDate, expiryDate, 0, 0);
-//        cq.allocateFoodToStore(food, date);
-//        assertThat(food.getPercentOfUsedShelfLife(date)).isEqualTo(Food.SHELF_LIFE_PERCENT_75);
-//        assertThat(shop.getFoods().size()).isEqualTo(1);
-//        assertThat(warehouse.getFoods().size()).isEqualTo(0);
-//        assertThat(trash.getFoods().size()).isEqualTo(0);
-//    }
-
-//    @Test
-//    void whenPercentOfUsedShelfLifeIsGreaterThan75AndLessThan100ThenFoodIsAllocatedToShop() {
-//        var createDate = LocalDateTime.of(2000, 1, 1, 12, 0);
-//        var expiryDate = LocalDateTime.of(2000, 1, 11, 12, 0);
-//        var date = LocalDateTime.of(2000, 1, 10, 12, 0);
-//        var food = new Food("foo", createDate, expiryDate, 0, 0);
-//        cq.allocateFoodToStore(food, date);
-//        assertThat(food.getPercentOfUsedShelfLife(date)).isGreaterThan(Food.SHELF_LIFE_PERCENT_75);
-//        assertThat(food.getPercentOfUsedShelfLife(date)).isLessThan(Food.SHELF_LIFE_PERCENT_100);
-//        assertThat(shop.getFoods().size()).isEqualTo(1);
-//        assertThat(warehouse.getFoods().size()).isEqualTo(0);
-//        assertThat(trash.getFoods().size()).isEqualTo(0);
-//    }
-
     @Test
     void whenPercentOfUsedShelfLifeIsGreaterThan75AndLessThan100ThenDiscountIsApplied() {
         var createDate = LocalDateTime.of(2000, 1, 1, 12, 0);
